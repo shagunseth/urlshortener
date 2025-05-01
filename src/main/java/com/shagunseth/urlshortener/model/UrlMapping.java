f078e3c92cc44f0f951bc92cac168b750f896bfc
Shagun Seth <sethshagun.s@gmail.com>
2025-04-30 22:16:30 -0400

    Spring Boot + MySQL setup

---
+++ src/main/java/com/shagunseth/urlshortener/model/UrlMapping.java
@@ -0,0 +1,29 @@
+package com.shagunseth.urlshortener.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String longUrl;
+
+    @Column(unique = true)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+}
